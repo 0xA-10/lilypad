238bc4936dd4ac5a48eac0a721cb19fb162b0965
0xA-10 <tony@natel.li>
2025-05-07 11:37:53 -0700

    reset

--- src/core/pipeline.ts
+++
@@ -1,6 +0,0 @@
-import { Segment, Ctx } from "./segments";
-
-export const compose =
-	(...pads: Segment[]): Segment =>
-	async (initial: Ctx = {}) =>
-		pads.reduce<Promise<Ctx>>((acc, pad) => acc.then(pad), Promise.resolve(initial));
