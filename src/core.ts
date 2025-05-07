83e4b6b05f6280f0b0e69d85f04e155563e313d4
0xA-10 <tony@natel.li>
2025-05-07 13:32:43 -0700

    feat: responses

--- src/core.ts
+++ src/core.ts
@@ -10,7 +10,6 @@ export type PipelineResult<In> = {
 	data: In;
 	transcript: Context<any>["transcript"];
 	session?: Context<any>["session"];
-	transformations: Transformation[];
 };
 
 /**
@@ -27,14 +26,8 @@ export function lilypad<In>(...steps: PromptStep<any, any>[]) {
 			let ctx: Context<unknown> = { data: input, transcript: [], session: undefined };
 			const transformations: Transformation[] = [];
 
-      for (const step of steps) {
-        const stepName = step.name ||
-        // cheaphax get fn name
-          new Error().stack!.split('at ')[3].split(' ')[0] || "<anonymous>";
-				const inputData = ctx.data;
+			for (const step of steps) {
 				const nextCtx = await step(ctx as any);
-				const outputData = nextCtx.data;
-				transformations.push({ step: stepName, inputData, outputData });
 				ctx = nextCtx;
 			}
 
@@ -42,7 +35,6 @@ export function lilypad<In>(...steps: PromptStep<any, any>[]) {
 				data: ctx.data,
 				transcript: ctx.transcript,
 				session: ctx.session,
-				transformations,
 			};
 		},
 	};
